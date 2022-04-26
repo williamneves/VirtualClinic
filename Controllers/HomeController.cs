42830efbc1605f9d8ac4f767614e8c6f621971fb
William Neves <drwilliamneves@gmail.com>
2022-04-26 03:46:31 -0400

    Create Auto-Save MedicalNote.cs,
    Show the notes with spaces and new lines
    
    fix z-index,

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -95,6 +95,49 @@ namespace VirtualClinic.Controllers
         {
             return View();
         }
+        // Post Medical Notes Model
+        [HttpPost("add-medical-notes-autosave")]
+        public IActionResult AddMedicalNotesAutoSave(MedicalNote newNote)
+        {
+            Console.WriteLine("\n###############\n");
+            Console.WriteLine("New Medical Note: ");
+            Console.WriteLine(newNote.PatientId);
+            Console.WriteLine(newNote.AP);
+            // Console.WriteLine(;
+            // Console.WriteLine(HPI);
+            Console.WriteLine("\n###############\n");
+                // dbContext.MedicalNotes.Add(newMedicalNote);
+                // dbContext.SaveChanges();
+                
+            var newNoteInDb = dbContext.MedicalNotes
+                .FirstOrDefault(m => m.PatientId == newNote.PatientId 
+                                     && m.ProviderId == newNote.ProviderId
+                                     && m.AppointmentId == newNote.AppointmentId);
+            if (newNoteInDb != null)
+            {
+                newNoteInDb.HPI = newNote.HPI;
+                newNoteInDb.PE = newNote.PE;
+                newNoteInDb.Summary = newNote.Summary;
+                newNoteInDb.AP = newNote.AP;
+                
+                // save changes
+                dbContext.SaveChanges();
+                
+                var response = "Auto-updated Medical Notes";
+            
+                return Json(response);
+            }
+            else
+            {
+                dbContext.MedicalNotes.Add(newNote);
+                dbContext.SaveChanges();
+                
+                var response = "Added new Medical Notes";
+            
+                return Json(response);
+            }
+
+        }
         
         // Json veiw for Medical Notes
         [HttpGet("json/medicalnotes/{id}")]
@@ -181,6 +224,23 @@ namespace VirtualClinic.Controllers
                 .Where(p => p.PatientId == userInDb.UserId && p.DateTime >= DateTime.Now.AddHours(-1))
                 .OrderBy(p => p.DateTime)
                 .FirstOrDefault();
+            
+            // Appointment with Id 1 and all includes
+            ViewBag.Appointment1 = dbContext.Appointments
+                .Include(p => p.Patient)
+                .Include(p => p.Provider)
+                .ThenInclude(u => u.User)
+                .Include(p => p.MedicalNotes)
+                .FirstOrDefault(p => p.AppointmentId == 1);
+            
+            ViewBag.MedicalNote1 = dbContext.MedicalNotes
+                .FirstOrDefault(m => m.PatientId == 1 
+                                     && m.ProviderId == 1
+                                     && m.AppointmentId == 1);
+            Console.WriteLine(ViewBag.MedicalNote1.HPI);
+            Console.WriteLine(ViewBag.MedicalNote1.PE);
+            Console.WriteLine(ViewBag.MedicalNote1.Summary);
+            Console.WriteLine(ViewBag.MedicalNote1.AP);
             
             return View();
         }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -76,3 +76,133 @@ function getMedicalNotes(id){
 
     });
 }
+
+// Auto Save Medical Notes
+// Set Interval function variable
+let auto_refresh;
+
+// Catch the form submit forms by .text-area-notes
+document.querySelectorAll('.text-area-notes').forEach(function(element) {
+    // Add event listener Focus to each element, to catch the focus event and start the auto save
+    element.addEventListener('focus', function(evt) {
+        autosaveForm(true);
+    });
+    
+    // Add event listener Blur to each element, to catch the blur event and stop the auto save
+    element.addEventListener('blur', function(evt) {
+        autosaveForm(false);
+    });
+});
+
+
+
+// Auto Save Function 
+// Set parameter run to true or false, set parameter FormID to the ID of the form, set the parameter interval to the interval of the auto save
+function autosaveForm(run = false,FormID = "form-medical-note-pd", interval = 500 ) {
+    
+    // If run is true, start the auto save
+    if (run) {
+    auto_refresh = setInterval(
+        function()
+        {
+            post();
+        }, interval);
+    }
+    // If run is false, stop the auto save
+    else {
+        clearInterval(auto_refresh);
+    }
+    
+    // Function to Save the form (if is new, backend will create a new record, if is update, backend will update the record)
+    function post(){
+        
+        let form = document.getElementById(FormID);
+        let formAction = form.getAttribute("action");
+        let formMethod = form.getAttribute("method");
+        
+        // Using Axios js library to make the post request
+        axios({
+            method: formMethod,
+            url: formAction,
+            data: $(form).serialize(),
+            dataType: 'json',
+        })
+            .then((res) => {
+                // console.log(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+    
+}
+
+// Auto Save Medical Notes - End
+
+// // autosaveForm();
+//
+// // Add event listener for all selector
+//
+//
+//
+// MedicalNoteForm.addEventListener("submit", function (event) {
+//   event.preventDefault();
+//   let form = event.target;
+//   let formData = new FormData(form);
+//   let formAction = form.getAttribute("action");
+//   let formMethod = form.getAttribute("method");
+//   let formDataObj = {};
+//   formData.forEach((value, key) => {
+//     formDataObj[key] = value;
+//     console.log("Update");
+//   });
+//   axios({
+//     method: formMethod,
+//     url: formAction,
+//     data: $(MedicalNoteForm).serialize(), 
+//       dataType: 'json',
+//   })
+//     .then((res) => {
+//       console.log(res);
+//       if (res.data.success) {
+//         // Success
+//         let successMessage = res.data.message;
+//         let successClass = "alert-success";
+//         let successId = "success-message";
+//         let successElement = createAlert(
+//           successMessage,
+//           successClass,
+//           successId
+//         );
+//         form.insertAdjacentElement("afterend", successElement);
+//         setTimeout(() => {
+//           successElement.remove();
+//         }, 3000);
+//       } else {
+//         // Error
+//         let errorMessage = res.data.message;
+//         let errorClass = "alert-danger";
+//         let errorId = "error-message";
+//         let errorElement = createAlert(
+//           errorMessage,
+//           errorClass,
+//           errorId
+//         );
+//         form.insertAdjacentElement("afterend", errorElement);
+//         setTimeout(() => {
+//           errorElement.remove();
+//         }, 3000);
+//       }
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// });
+// // function submitmedicalnote(event){
+// //     console.log(event.target.HPI);
+// //     console.log(event.target.PE);
+// //     console.log(event.target.Summary);
+// // }
+
+
+
